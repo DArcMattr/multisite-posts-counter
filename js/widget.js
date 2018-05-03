6194df23577a5a85c750afc4c53cd76d20b44728
David Arceneaux <david@davidthemachine.org>
2018-05-03 15:05:33 -0700

    get linter working, fix linter messages

--- js/widget.js
+++ js/widget.js
@@ -1,24 +1,24 @@
-(function($){
-	let refreshTime = $( '.mpc-list' ).data( 'refresh' ),
+( function( $ ) {
+	var refreshTime = $( '.mpc-list' ).data( 'refresh' ),
 		poll = window.setInterval(
 			function() {
-				let endpointUrl = $( 'link[rel="https://api.w.org/"]' ).attr( 'href' ) +
-					$( '.mpc-list' ).data('endpoint' );
+				var endpointUrl = $( 'link[rel="https://api.w.org/"]' ).attr( 'href' ) +
+					$( '.mpc-list' ).data( 'endpoint' );
 
 				$.ajax( endpointUrl )
 					.done( function( data ) {
 						$.each( data, function( blogId, blogInfo ) {
-							$('.mpc-list-item[data-blog_id=' + blogId + '] .mpc-list-item-link')
-								.attr('href', blogInfo['url'] )
-								.text( blogInfo['name'] );
-							$('.mpc-list-item[data-blog_id=' + blogId + '] .mpc-list-item-posts')
-								.text( blogInfo['post_count'] );
-							$('.mpc-list-item[data-blog_id=' + blogId + '] .mpc-list-item-users')
-								.text( blogInfo['user_count'] );
-						} )
-					} )
+							$( '.mpc-list-item[data-blog_id=' + blogId + '] .mpc-list-item-link' )
+								.attr( 'href', blogInfo.url )
+								.text( blogInfo.name );
+							$( '.mpc-list-item[data-blog_id=' + blogId + '] .mpc-list-item-posts' )
+								.text(  blogInfo.post_count );
+							$( '.mpc-list-item[data-blog_id=' + blogId + '] .mpc-list-item-users' )
+								.text( blogInfo.user_count );
+						});
+					});
 				},
 			refreshTime * 1000
 		);
-})(jQuery);
+}( jQuery ) );
 
